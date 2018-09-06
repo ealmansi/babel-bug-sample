a89a9849305cfb9932ed68c2759fcc1ef87e65d9
Emilio Almansi <ealmansi@gmail.com>
2018-09-06 17:55:32 -0300

    Simplified example.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-var async = require('async');
+var async = require('async')
 var util = require('util')
-var mapLimit = util.promisify(async.mapLimit)
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-;(async () => {
-  const res = await mapLimit(numbers, 3, async function(number) {
-      return number
-  })
-  console.log('res', res)
-})()
+var numbers = [1, 2, 3, 4, 5]
+new Promise(resolve => {
+  async.eachLimit(
+    numbers,
+    1,
+    async number => {
+      console.log('number', number)
+    },
+    resolve
+  )
+})
